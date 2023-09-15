e0dccc460a23760615e18bc9e9d1dbb782734269
atlas-core <58980524+atlas-core@users.noreply.github.com>
2023-09-15 13:35:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const http = require('http');
-const { chromium } = require('playwright');
+let playwright;
+let chromium;
 
-http.createServer(async function (req, res) {
-    try {
-        // Launch a Playwright browser instance
-        const browser = await chromium.launch();
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // Running on AWS Lambda.
+  chromium = require('chrome-aws-lambda');
+  playwright = require('playwright-aws-lambda'); // Playwright with AWS Lambda support
+} else {
+  // Running locally.
+  playwright = require('playwright');
+}
 
-        // Create a new page
-        const page = await browser.newPage();
-
-        // Navigate to Google's homepage
-        await page.goto('https://www.google.com');
-
-        // Take a screenshot of the page
-        const screenshot = await page.screenshot();
-
-        // Close the Playwright browser
-        await browser.close();
-
-        // Set the response headers
-        res.writeHead(200, {
-            'Content-Type': 'image/png',
-            'Content-Length': screenshot.length
-        });
-
-        // Send the screenshot as the response
-        res.end(screenshot);
-    } catch (error) {
-        console.error('Error:', error);
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error: ' + error.message);
+exports.handler = async (event, context) => {
+  let browser = null;
+  try {
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+      browser = await playwright.chromium.launch({
+        executablePath: await chromium.executablePath,
+        args: chromium.args,
+      });
+    } else {
+      browser = await playwright.chromium.launch();
     }
-}).listen(process.env.PORT || 3000, () => {
-    console.log('Server is running on port', process.env.PORT || 3000);
-});
+
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    // Your scraping or automation logic here
+
+    return {
+      statusCode: 200,
+      body: 'Scraping completed successfully.',
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error',
+    };
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+};
