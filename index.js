02101ca6ae7cf95a572ea00d6c69f8352e5bcaef
atlas-core <58980524+atlas-core@users.noreply.github.com>
2023-09-15 13:12:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
+const http = require('http');
+const puppeteer = require('puppeteer');
+
+http.createServer(async function (req, res) {
+    // Create a Puppeteer browser instance
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Navigate to a webpage
+    await page.goto('https://example.com');
+
+    // Take a screenshot of the page
+    const screenshot = await page.screenshot();
+
+    // Close the Puppeteer browser
+    await browser.close();
+
+    // Set the response headers
+    res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': screenshot.length
+    });
+
+    // Send the screenshot as the response
+    res.end(screenshot);
 }).listen(process.env.PORT || 3000);
